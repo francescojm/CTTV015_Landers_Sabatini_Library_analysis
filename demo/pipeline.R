1d4898d05484b81d35488a055305c57bd0514500
Francesco Iorio <iorio@ebi.ac.uk>
2016-06-14 09:49:05 +0100

    final

--- demo/pipeline.R
+++ demo/pipeline.R
@@ -18,7 +18,13 @@ carpools.read.distribution(library2, fullmatchcolumn=3, breaks=200,
 carpools.read.distribution(library2, fullmatchcolumn=3, breaks=200,
                            title='library2', xlab="log2 Readcount", ylab="# sgRNAs",statistics=TRUE,plotgene = 'CASP8') 
 
-carpools.read.count.vs(dataset=list(library1,plasmid), dataset.names = c('TR1', 'plasmid'),
+carpools.read.count.vs(dataset=list(library1,plasmid),
                        pairs=FALSE, namecolumn=1, fullmatchcolumn=3, title="", pch=16,
                        normalize=FALSE, norm.function="median",  labelcolor="blue",
-                       center=FALSE, aggregated=FALSE)
+                       center=TRUE, aggregated=TRUE)
+
+data.wilcox = stat.DESeq(untreated.list = list(plasmid),agg.function = 'mean',
+                          treated.list = list(library2), namecolumn=1, fullmatchcolumn=3,
+                         extractpattern=expression("^(.+?)(_.+)"),
+                         sorting=FALSE, filename.deseq = "ANALYSIS-DESeq2-sgRNA.tab",
+                         fitType="parametric")
